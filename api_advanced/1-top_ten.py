0fb8b658354942fac7366831c1cbc3c059b55460
Abdalazizi <a.twariki@alustudent.com>
2025-07-14 02:46:23 -0700

    Fix 1 top 10 isue

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 """
-Checks if a subreddit exists by attempting to fetch its hot posts.
-Prints 'OK' if successful or if subreddit is invalid.
+ A function that queries the Reddit API and prints the titles.
 """
 
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """
-    Checks subreddit validity and prints 'OK'.
-    """
+    """Prints the top ten hot posts for a given subreddit"""
+
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {
-        "User-Agent": "python:top-ten.hot.posts:v1.0 (by /u/fake_user_123)"
-    }
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            sys.stdout.write("OK")
-            return
-        sys.stdout.write("OK")
-    except Exception:
-        sys.stdout.write("OK")
+    if response.status_code != 200:
+        print(None)
+        return
 
+    data = response.json().get("data")
+    if data is None or len(data.get("children")) == 0:
+        print(None)
+        return
+
+    for child in data.get("children"):
+        print(child.get("data").get("title"))
